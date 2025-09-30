b3ee363601d66225abc98903e42a6b28b4e0ae8b
piss0 <hafizrisk425@gmail.com>
2025-09-30 13:55:16 +0700

    belajar do while

--- do while.dart
+++ do while.dart
@@ -31,10 +31,62 @@ void main(List<String> args) {
  * dengan kelipatan 3 menggunakan while
  * 3,6,9,12,15,18,21,24,27,30
  */
- var kelipatan = 3;
+  var kelipatan = 3;
 
   while (kelipatan <= 30) {
-    print('ini adalah angka kelipatan $kelipatan');
+    print("ini adalah angka kelipatan $kelipatan");
     kelipatan += 3;
   }
+
+  /**
+   * lakukan print
+   * sertakan print (program berjalan)
+   * lakukan pengecekan 0 <= 30 ? bukan?
+   * ya 0 <=, maka tambahkan 0+3
+   * lakukan print,
+   * setelah print lakukan pengecekan 3 <=, bukan?
+   * ya 3 <= 30, maka 3+= 3 (3+3)
+   */
+
+  /** 
+   * buatkan angka yang menampilkan kelipatam 7, dan
+   * jumlahkan total kelipatan nya var kelipatantujuh
+   * range 1-100
+   * 7,14,.....98
+   * 7+14+21 .....+98 = ?
+   * total nya berapa?
+   */
+  var kelipatantujuh = 7;
+  var total = 0;
+
+  while (kelipatantujuh <= 100) {
+    print("ini adalah angka $kelipatantujuh");
+    total += kelipatantujuh;
+    kelipatantujuh += 7;
+  }
+
+  print("===========");
+  print("Total jumlah kelipatan 7 adalah $total");
+
+  var bebas = 7;
+  var ttl = 0;
+  do {
+    print("ini adalah angka $bebas");
+    ttl += bebas;
+    bebas += 7;
+  } while (bebas <= 100);
+
+  print("============");
+  print("total nya adalah $ttl");
+
+  /**
+   * loop angka dari 50 - 20
+   * menggunakan do while
+   */
+
+  var angkaa = 51;
+  do {
+    angkaa--;
+     print('ini adalah $angkaa');
+  } while (angkaa >= 21);
 }
